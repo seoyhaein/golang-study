1da5aafb86cdcfbf8ef8db792fdcf47daefe5484
seoyhaein <seoy@bio-caleb.com>
2021-10-30 16:26:55 +0900

    grpc server 수정-추후 개선 및 기타 잡코드(추후 삭제)

--- v1rpc/grpc.go
+++ v1rpc/grpc.go
@@ -38,15 +38,17 @@ func init() {
 // tls 는 일단 지금은 생략하자.
 // 추후 고쳐나가자. 일단은 가장 간단한 형태로 만들어 놓고 테스트 진행하면서 고도화 하자.
 // gopts ...grpc.ServerOption 집어 넣는 방향
-func Server() error {
-
-	var err error = nil
+func Server() (err error) {
+	// 10/30
+	// 질문: 아래 주석들 return 과 관련해서, 주석을 해지 했을때와 현제 코드의 차이점은?
+	// var err error = nil
 	network = "tcp"
 	address = ":50052"
 
 	lis, err = net.Listen(network, address)
 	if err != nil {
-		return err
+		return
+		//return err
 	}
 
 	var opts []grpc.ServerOption
@@ -58,7 +60,7 @@ func Server() error {
 	// 일단 이걸 교체해야함.
 	// 테스트 용으로 hello world 이용함.
 	// 10/30
-	//TODO api 와 package 를 달리 해서 헷갈리는 것을 방지 하자
+	//TODO api 와 package 를 달리 해서 헷갈리는 것을 방지 하자 10/30
 	RegisterHello(grpcServer)
 
 	log.Println("gRPC server started, address : ", address, "listener-address", lis.Addr().String())
@@ -70,5 +72,6 @@ func Server() error {
 	} else {
 		log.Println("gRPC server returned with error , address :", address)
 	}
-	return err
+	return
+	//return err
 }
